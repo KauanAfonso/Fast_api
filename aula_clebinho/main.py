8602c02db1bd92639078ddbaf0b33b1a80244766
kauanAfonso <kauanafon3@gmail.com>
2025-04-14 16:58:17 -0300

    Aula de fastapi, continuação da docs. Hoje vimos query paramters, baseModel e union

--- aula_clebinho/main.py
+++ aula_clebinho/main.py
@@ -4,8 +4,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+
 #@app -> é um decorador, ou seja ele ira pegar a função de baixo e retorna-la como um função para o fastapai
-@app.get("/kauan")
+@app.get("/")
 async def read_root(): #aync dispara coisas com tempo independentes
     return {"Hello": "World"}
 
@@ -71,3 +73,56 @@ async def item_name(name:str, age: int | None = None, angry: bool = False):
         return {"message": msg}
     msg = f"My name is {name}"
     return {'msg': msg}
+
+from typing import Union
+from pydantic import BaseModel
+
+
+#Base model
+class Item(BaseModel):
+    name : str
+    description : Union[str, None] = None #ou será str ou none, e se ele não mandar nada será none
+    price: float
+    tax: Union[float,None] = None
+
+
+@app.post('/items/')
+async def create_item(item:Item):
+    print(item.model_dump)
+    return item
+
+
+# @app.put('/curso/{curso_id}')
+# asyn
+
+class Curso(BaseModel):
+    name:str
+    duration:int
+
+@app.put('/curso/{curso_id}')
+async def update_curso(curso_id: int, curso: Curso):
+    return{"curso_id": curso_id, **curso.model_dump()}
+
+
+#Função de query 
+from fastapi import Query
+
+@app.get("/items_query/")
+# Define um endpoint GET acessível na rota "/items_query/"
+async def read_items(
+    q: Union[str, None] = Query(
+        default=None,                     # O parâmetro é opcional (None por padrão)
+        max_length=50,                    # Tamanho máximo da string: 50 caracteres
+        min_length=3,                     # Tamanho mínimo da string: 3 caracteres
+        title="Titulo do item",           # Título do parâmetro na documentação automática (Swagger)
+        description="Descrição sobre o item deve ser STR",  # Descrição na documentação
+        deprecated=True,                  # Indica que esse parâmetro está obsoleto (aparece tachado na docs)
+        pattern="^[^\W\d_]{3}$"         # Expressão regular para validar o valor do parâmetro (⚠️ Tem um erro aqui)
+    )
+):
+    
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+   
