f565fbab82de5bcae8a9c516bbd4fa991d32e0ff
kauanAfonso <kauanafon3@gmail.com>
2025-04-28 16:52:41 -0300

    aula de fastapi clebinho

--- aula_clebinho/main.py
+++ aula_clebinho/main.py
@@ -6,6 +6,7 @@ app = FastAPI()
 
 
 
+'''
 #@app -> é um decorador, ou seja ele ira pegar a função de baixo e retorna-la como um função para o fastapai
 @app.get("/")
 async def read_root(): #aync dispara coisas com tempo independentes
@@ -58,7 +59,7 @@ fake_items_db = [
 ]
 
 
-#Query paramters
+#----------------------------------------------Query paramters
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
@@ -78,7 +79,7 @@ from typing import Union
 from pydantic import BaseModel
 
 
-#Base model
+#---------------------------------------Usando o Base model
 class Item(BaseModel):
     name : str
     description : Union[str, None] = None #ou será str ou none, e se ele não mandar nada será none
@@ -104,6 +105,10 @@ async def update_curso(curso_id: int, curso: Curso):
     return{"curso_id": curso_id, **curso.model_dump()}
 
 
+
+
+#---------------------------------QUERY PARAMTERS
+
 #Função de query 
 from fastapi import Query
 
@@ -116,13 +121,247 @@ async def read_items(
         min_length=3,                     # Tamanho mínimo da string: 3 caracteres
         title="Titulo do item",           # Título do parâmetro na documentação automática (Swagger)
         description="Descrição sobre o item deve ser STR",  # Descrição na documentação
-        deprecated=True,                  # Indica que esse parâmetro está obsoleto (aparece tachado na docs)
+        deprecated=False,                  # Indica que esse parâmetro está obsoleto (aparece tachado na docs)
         pattern="^[^\W\d_]{3}$"         # Expressão regular para validar o valor do parâmetro (⚠️ Tem um erro aqui)
     )
 ):
     
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
-        results.update({"q": q})
+        results.update({"q": q}) #adicionou o q no results
     return results
    
+
+#enviando varios valores
+@app.get("/mulitple_items/")
+async def read_items(q: list[str] | None = Query(default=None, title="Valor de Consulta nulo", description="Olá eu sou a descricao")):
+    query_items = {'q':q}
+    return query_items #ex:http://127.0.0.1:8000/items/?q=Kauan&q=joao
+
+
+
+from typing import Annotated
+
+#query parameters com parametros default
+@app.get("/items_default/")
+async def read_items(q: Annotated[list[str], Query(max_length=50)] = ['foo', 'baa']): #anonotated força ter mais de um tipo
+    query_itens = ({"q": q})
+    return query_itens
+
+
+#-----------------------Parametros de validação númericas
+
+
+from typing import Annotated
+
+from fastapi import FastAPI, Path
+
+app = FastAPI()
+
+
+#------------Intervalor de valores
+# ge = maior ou igual
+#gt = maior que
+#le = menor ou igual
+#lt = menor que
+
+@app.get("/items_number/{item_id}")
+async def read_items(
+    item_id: Annotated[int, Path(title="The ID of the item to get", ge=99, le=67)], q: str):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+'''
+
+'''
+#------------modelo de parametris de consulta -> 
+
+from typing import Annotated, Literal
+
+from fastapi import FastAPI, Query
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+class FilterParams(BaseModel):
+    limit: int = Field(100, gt=0, le=100)
+    offset: int = Field(0, ge=0)
+    order_by: Literal["created_at", "updated_at"] = "created_at" #pode ter um dos dois valores / valor padrao
+    tags: list[str] = [] #lista de 
+
+
+@app.get("/items/")
+async def read_items(filter_query: Annotated[FilterParams, Query()]):
+    return filter_query
+
+
+
+from typing import Annotated, Literal
+
+from fastapi import FastAPI, Query
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+class FilterParams(BaseModel):
+    model_config = {"extra": "forbid"}#Se ele tentar enviar um parametro adicional ->traga essa mensagem
+
+    limit: int = Field(100, gt=0, le=100)
+    offset: int = Field(0, ge=0)
+    order_by: Literal["created_at", "updated_at"] = "created_at"
+    tags: list[str] = []
+
+
+@app.get("/items/")
+async def read_items(filter_query: Annotated[FilterParams, Query()]):
+    return filter_query
+
+
+
+
+#----------------------------------Parametros de Consulta no body----------
+# Parametros diferefentes e um deles vem do corpo
+from typing import Annotated
+
+from fastapi import FastAPI, Path
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
+    #ou passa como parametro uma string ou um item
+    q: str | None = None,
+    item: Item | None = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if item:
+        results.update({"item": item})
+    return results
+
+
+
+
+#---------------------Multiplos parametros enviando no copor da requisião automatico
+
+from typing import Annotated
+
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: int, importance: Annotated[int, Body()] = 0
+    ):
+    # a variavel importance nesse caso será enviada no corpo da requisição
+    results = {
+        "item_id": item_id, 
+        "importance": importance
+        }
+    return results
+
+
+
+
+#-----------------------Body fields---------------------
+#Seria o msm que o query mas para o corpo da requisição -> ou seja, define o que e como precisa ser enviado!
+
+from typing import Annotated
+
+from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = Field(
+        default=None, title="The description of the item", max_length=300
+    )
+    price: float = Field(gt=0, description="The price must be greater than zero")
+    tax: float | None = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+
+
+#------------------Tipo set -> Um tipo de dado que não te permite repetir valores
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: set[str] = set()
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+'''
+
+#-----------------------------------Um dicionario dentro do outro(modelo aninhado) 
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Image(BaseModel):
+    url: str
+    name: str
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: set[str] = set()
+    image: Image | None = None #Aqui acontece isso.
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
