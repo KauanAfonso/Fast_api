748ba86352e9010e73a9ff6789d7fe0f044503a0
kauanAfonso <kauanafon3@gmail.com>
2025-06-04 14:29:01 -0300

    crd local

--- data_ds16/api/v1/api.py
+++ data_ds16/api/v1/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from api.v1.endpoints import profissao
+from api.v1.endpoints import profissao, local
 
 api_router = APIRouter()
 
 api_router.include_router(profissao.router, prefix="/profissao", tags=["Profissão"])
+api_router.include_router(local.router, prefix="/local", tags=["Local"])
--- data_ds16/api/v1/endpoints/local.py
+++ data_ds16/api/v1/endpoints/local.py
@@ -4,10 +4,71 @@ from sqlalchemy.ext.asyncio import AsyncConnection
 from sqlalchemy.future import select
 from models.local_model import LocalModel
 from schemas.local_shemas import LocalSchema
-from core.deps import getSession
+from core.deps import get_session
 
 router = APIRouter()
 
 @router.post("/", status_code=status.HTTP_200_OK, response_model=LocalSchema)
-async def criar_local(local:LocalSchema, db: AsyncConnection = Depends(getSession)):
-    pass
+async def post_local(local:LocalSchema, db: AsyncConnection = Depends(get_session)):
+    novo_local = LocalModel(
+        nome=local.nome,
+        relacao=local.relacao,
+        foto=local.foto
+    )
+
+    db.add(novo_local)
+    await db.commit()
+    return novo_local
+
+@router.post("/", response_model=list[LocalSchema])
+async def get_locais(db: AsyncConnection = Depends(get_session)):
+    async with db as session:
+        query = select(LocalModel)
+        result = await session.execute(query)
+        locais: list[LocalModel] = result.scalars.all()
+
+        return locais
+
+@router.get("/{local_id}", response_model=LocalSchema)
+async def get_local(local_id: int, db: AsyncConnection = Depends(get_session)):
+    async with db as session:
+        query = select(LocalModel).filter(LocalModel.id == local_id)
+        result = await session.execute(query)
+        local = result.scalar_one_or_none()
+
+        if local:
+            return local
+        raise HTTPException(detail="Local não encontrado !", status_code=status.HTTP_404_NOT_FOUND)
+    
+@router.put("/{local_id}", response_model=LocalSchema)
+async def put_local(local_id: int, local: LocalSchema, db: AsyncConnection = Depends(get_session)):
+    async with db as session:
+        query = select(LocalModel).filter(LocalModel.id == local.id)
+        result = await session.execute(query)
+        local_up = result.scalar_one_or_none()
+
+        if local_up:
+            local_up.nome = local.nome
+            local_up.relacao = local.relacao
+            local_up.foto = local.foto
+
+            session.add(local_up)
+            await session.commit()
+            return local_up
+        
+        else:
+            raise HTTPException(detail="Local não encontrada", status_code=status.HTTP_404_NOT_FOUND)
+
+@router.delete("/{local_id}", response_model=LocalSchema, status_code=status.HTTP_404_NOT_FOUND)
+async def delete_local(local_id:int, db:AsyncConnection = Depends(get_session)):
+    async with db as session:
+        query = select(LocalModel).filter(LocalModel.id == local_id)
+        result = await session.execute(query)
+        local_delete = result.scalar_one_or_none()
+
+        if local_delete:
+            await session.delete(local_delete)
+            await session.commit()
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+        else:
+            raise HTTPException(detail="Local não encontrado", status_code=status.HTTP_404_NOT_FOUND)
